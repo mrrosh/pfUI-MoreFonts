c596819c4e5c8f58b823bb740fd70be94bc43978
Mr_Rosh <cosminrosca1995@gmail.com>
2023-10-21 13:38:57 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# pfUI-fonts
+
+A font package for pfUI, providing additional fonts from the google font project.
+
+## Installation (Vanilla, 1.12)
+1. Download **[Latest Version](https://github.com/mr_rosh/pfUI-MoreFonts/archive/master.zip)**
+2. Unpack the Zip file
+3. Rename the folder "pfUI-MoreFonts-master" to "pfUI-MoreFonts"
+4. Copy "pfUI-MoreFonts" into Wow-Directory\Interface\AddOns
+5. Restart Wow
+
+## Installation (The Burning Crusade, 2.4.3)
+1. Download **[Latest Version](https://github.com/mr_rosh/pfUI-MoreFonts/archive/master.zip)**
+2. Unpack the Zip file
+3. Rename the folder "pfUI-MoreFonts-master" to "pfUI-MoreFonts-tbc"
+4. Copy "pfUI-MoreFonts-tbc" into Wow-Directory\Interface\AddOns
+5. Restart Wow
+
+## Preview
+![thumbnails](https://i.imgur.com/WvSA0r6.png)
---
+++ pfUI-MoreFonts.lua
@@ -0,0 +1,22 @@
+local addonpath
+local tocs = { "", "-master", "-tbc", "-wotlk" }
+for _, name in pairs(tocs) do
+  local current = string.format("pfUI-MoreFonts%s", name)
+  local _, title = GetAddOnInfo(current)
+  if title then
+    addonpath = "Interface\\AddOns\\" .. current
+    break
+  end
+end
+
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\Acme-Regular.ttf:Acme")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\AlegreyaSansSC-Bold.ttf:AlegreyaSansSC")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\Bangers-Regular.ttf:Bangers")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\BarlowCondensed-Medium.ttf:BarlowCondensed")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\BigNoodleTitlingOblique.ttf:BigNoodleTitlingOblique")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\FjallaOne-Regular.ttf:FjallaOne")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\FTY.ttf:FTY")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\PathwayGothicOne-Regular.ttf:PathwayGothicOne")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\RoadRage-Regular.ttf:RoadRage")
+table.insert(pfUI.gui.dropdowns.fonts, addonpath .. "\\fonts\\US101.ttf:US101")
+
